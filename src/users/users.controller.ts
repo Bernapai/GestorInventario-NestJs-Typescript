01fd1010cf737273bdb8da1f535fa8094388a4e6
Bernapai <juanemeunier@gmail.com>
2025-03-25 20:43:54 -0300

    Service and Controller for User Finished

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import User from './users.entity';
+import { UserUpdateDto } from './dto/userUpdate.dto';
+import { UserCreateDto } from './dto/userCreate.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+  // Obtener todos los usuarios
+  @Get()
+  async getAll(): Promise<User[]> {
+    return await this.usersService.getAll();
+  }
+
+  // Obtener un usuario por ID
+  @Get(':id')
+  async getOne(@Param('id') id: number): Promise<User | null> {
+    return await this.usersService.getOne(id);
+  }
+
+  // Actualizar un usuario por ID
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() data: UserUpdateDto, // Usamos el DTO UpdateUserDto aquí
+  ): Promise<User | null> {
+    return await this.usersService.update(id, data);
+  }
+
+  @Post()
+  async register(@Body() data: UserCreateDto): Promise<User> {
+    return await this.usersService.register(data);
+  }
+
+  // Eliminar un usuario por ID
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<boolean> {
+    return await this.usersService.delete(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  // Obtener todos los usuarios
+  async getAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async register(data: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(data);
+    return await this.userRepository.save(newUser);
+  }
+
+  // Obtener un solo usuario por ID
+  async getOne(id: number): Promise<User | null> {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  // Obtener un solo usuario por nombre
+  async findByName(name: string): Promise<User | null> {
+    return await this.userRepository.findOne({ where: { name } });
+  }
+
+  // Actualizar un usuario por ID
+  async update(id: number, data: Partial<User>): Promise<User | null> {
+    await this.userRepository.update(id, data);
+    return this.getOne(id);
+  }
+
+  // Eliminar un usuario por ID
+  async delete(id: number): Promise<boolean> {
+    const result = await this.userRepository.delete(id);
+    return (result.affected ?? 0) > 0;
+  }
+}
