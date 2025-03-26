65cbf75080e88a035b473613f7896a8d01c613ac
Bernapai <juanemeunier@gmail.com>
2025-03-26 18:56:48 -0300

    Add TransactionsController Finished

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,12 +1,60 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Param,
-    Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
 } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import Transaction from './transactions.entity';
+import { TransactionCreateDto } from './dto/transactionsCreate.dto';
+import { TransactionUpdateDto } from './dto/transactionsUpdate.dto';
 
 @Controller('transactions')
-export class TransactionsController { }
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) { }
+
+  @Get()
+  async getAll(): Promise<Transaction[]> {
+    return await this.transactionsService.getAll();
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: number): Promise<Transaction | null> {
+    return await this.transactionsService.getOne(id);
+  }
+
+  @Post()
+  async create(@Body() data: TransactionCreateDto): Promise<Transaction> {
+    return await this.transactionsService.create(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() data: TransactionUpdateDto,
+  ): Promise<Transaction> {
+    return await this.transactionsService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return await this.transactionsService.delete(id);
+  }
+
+  @Get(':id/products')
+  async getProducts(@Param('id') id: number): Promise<Transaction | null> {
+    try {
+      const products = await this.transactionsService.getProducts(id);
+      if (products instanceof Transaction || products === null) {
+        return products;
+      }
+      throw new Error('Invalid return type');
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      throw new Error('Failed to fetch products');
+    }
+  }
+}
